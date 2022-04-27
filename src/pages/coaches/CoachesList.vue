03d11953cf5e44f59305e331f51344b87067d898
Zizoko <zizoko1337@gmail.com>
2022-04-27 22:50:52 +0200

    Added dummy coaches and their getters,  display them inside listCoaches

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,9 +1,28 @@
 <template>
-  <section>
-    FILTERe
-  </section>
+  <section>FILTERe</section>
   <section>
     LIST OF COACHES
     <router-link to="/coaches/c1">Example coach</router-link>
+    <div>
+      <ul v-if="hasCoaches">
+        <li v-for="coach in filteredCoaches" :key="coach.id">
+          {{ coach.firstName }}
+        </li>
+      </ul>
+      <h3 v-else>No coaches found.</h3>
+    </div>
   </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters['coaches/coaches']; // [ 'namespace name from ./store/index.js', 'getter name']
+    },
+    hasCoaches() {
+      return this.$store.getters['coaches/hasCoaches']; 
+    },
+  },
+};
+</script>
---
+++ src/store/modules/coaches/index.js
@@ -0,0 +1,42 @@
+import mutations from './mutations.js';
+import actions from './actions';
+import getters from './getters';
+
+export default {
+  namespaced: true, // it helps with separating names of actions,getters etc. from different modules. So there will be no name conflicts.
+  state() {
+    return {
+      coaches: [
+        {
+          id: 'c1',
+          firstName: 'Maximilian',
+          lastName: 'Schwarzmüller',
+          areas: ['frontend', 'backend', 'career'],
+          description:
+            "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
+          hourlyRate: 30,
+        },
+        {
+          id: 'c2',
+          firstName: 'Julie',
+          lastName: 'Jones',
+          areas: ['frontend', 'career'],
+          description:
+            'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
+          hourlyRate: 30,
+        },
+        {
+          id: 'c3',
+          firstName: 'Mateusz',
+          lastName: 'Król',
+          areas: ['frontend', 'backend'],
+          description: 'I am Mateusz and i love my dog Boguś 🐶.',
+          hourlyRate: 30,
+        },
+      ],
+    };
+  },
+  mutations: mutations, // one 'mutations' would be enough because we import it with the same name
+  actions,
+  getters,
+};
